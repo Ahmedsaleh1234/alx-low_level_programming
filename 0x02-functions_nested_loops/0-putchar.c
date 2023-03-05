5a61c3a706ab9b0dc92445a97e351b96037595e1
Ahmedsaleh1234 <a12661826@gmail.com>
2023-03-05 14:41:16 -0800

    0-putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * main - check the code for Holberton School students.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+char letter[] = "_putchar";
+int i;
+
+for (i = 0; i < 9; i++)
+{
+_putchar(letter[i]);
+}
+
+_putchar('\n');
+
+return (0);
+}
+
