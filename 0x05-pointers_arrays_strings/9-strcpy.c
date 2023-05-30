5b275174cd21fc316e5a4b4185060ebe0e948cc9
Ahmedsaleh1234 <a12661826@gmail.com>
2023-05-30 01:49:10 -0700

    9-strcpy.c

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ *_strcpy- copies string
+ *@dest:checked
+ *@src: checked
+ *Return: dest
+ */
+char *_strcpy(char *dest, char *src)
+{
+int i;
+i = 0;
+while (src[i] != '\0')
+{
+dest[i] = src[i];
+i++;
+}
+return (dest);
+}
