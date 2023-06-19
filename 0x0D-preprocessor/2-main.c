8716afd614f81c3d83fd0b4b13757fe69558a7c4
Ahmedsaleh1234 <a12661826@gmail.com>
2023-06-19 07:59:13 -0700

    2-main.c

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - print the name of currunt file
+ *
+ * Return: Always 0 Sucess
+ */
+int main(void)
+{
+printf("the name of the file = %s\n", __FILE__);
+
+return (0);
+}
