b50d885ce282e5eaec545b53bf612b3ce42a9daf
Ahmedsaleh1234 <a12661826@gmail.com>
2023-06-21 15:15:13 -0700

    3-main.c, 3-op_functions.c, 3-get_op_func.c, 3-calc.h

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,51 @@
+#include "3-calc.h"
+/**
+ * op_add - add two number
+ * @a: number1
+ * @b: number2
+ * Return: tehe sum of two number.
+ */
+int op_add(int a, int b)
+{
+return (a + b);
+}
+/**
+ * op_sub - subtract two number
+ * @a: frist number
+ * @b: scound number
+ * Return: the subtract of two number
+ */
+int op_sub(int a, int b)
+{
+return (a - b);
+}
+/**
+ * op_mul - mult two number
+ * @a: frist number
+ * @b: scound number
+ * Return: the mult of two number
+ */
+int op_mul(int a, int b)
+{
+return (a * b);
+}
+/**
+ * op_div - division  two number
+ * @a: frist number
+ * @b: scound number
+ * Return: the result of divison of two number
+ */
+int op_div(int a, int b)
+{
+return (a / b);
+}
+/**
+ * op_mod - calculates the remainder of  two number
+ * @a: frist number
+ * @b: scound number
+ * Return: the result of remaider of two number
+ */
+int op_mod(int a, int b)
+{
+return (a % b);
+}
