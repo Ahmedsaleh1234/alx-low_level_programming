4b681e056393dda5f822a63811db3f2f8468c770
Ahmedsaleh1234 <a12661826@gmail.com>
2023-07-10 14:31:10 -0700

    1-create_file.c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -7,26 +7,20 @@
  */
 int create_file(const char *filename, char *text_content)
 {
-int fd;
-int l = 0;
-fd = open(filename, O_RDWR | O_CREAT | O_TRUNC, 0600);
+int file_descriptor, length = 0;
+
 if (!filename)
+return (-1);
+file_descriptor = open(filename, O_RDWR | O_CREAT | O_TRUNC, 0600);
+if (file_descriptor < 0)
+return (-1);
+while (text_content && text_content[length])
+length++;
+if (write(file_descriptor, text_content, length) < 0)
 {
-return (0);
-}
-if (fd == -1)
-{
+close(file_descriptor);
 return (-1);
 }
-while (*text_content)
-{
-l++;
-}
-if (!l)
-{
-return (-1);
-}
-write(fd, text_content, l);
-close(fd);
+close(file_descriptor);
 return (1);
 }
